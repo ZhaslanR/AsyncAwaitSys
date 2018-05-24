9c96cb7a6cfa78e63641d5d02e8365244fb719ab
ZhaslanR <zhaslan_12@bk.ru>
2018-05-24 15:43:46 +0600

    Добавьте файлы проекта.

---
+++ AsyncAwait_Dz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncAwait_Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
